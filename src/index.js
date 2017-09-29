7bf81a15a8505985d91216e3514fd0ff1dd650d8
igor-provotorov <garynya@yandex.ru>
2017-09-29 11:30:19 +0300

    Complete task additional_5 - Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) return false;
+  var string = str.split('');
+  var stack = [];
+  for(var i = 0; i < str.length; i++){
+    for(var j = 0; j < bracketsConfig.length; j++){
+      if (string[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+        stack.pop();
+      } else if (string[i] == bracketsConfig[j][0]) {
+        stack.push(string[i]);
+      }
+
+    }
+  }
+
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
